843b17eded580d6243c1b067c4d362b9e9622d23
Matteus <post@kalis.no>
2020-09-21 12:26:10 +0200

    Add javascript monad example

---
+++ monad.js
@@ -0,0 +1,43 @@
+// Monads are containers of functions that you can execute *later*.
+// Those functions can take some time to run.
+// Once we call .preform() this function will be executed
+class Monad {
+  constructor(fn) {
+    this.fn = fn;
+  }
+
+  flatMap(fn) {
+    return new Monad(() => {
+      fn(this.fn());
+    });
+  }
+
+  preform() {
+    return this.fn();
+  }
+}
+
+const a = new Monad(() => {
+  console.log('Begin calculation...');
+  sleep(1);
+  const calculated = 'XXyyZZ';
+  console.log('Done!');
+  return calculated;
+});
+
+const b = a.flatMap((calculated) => {
+  console.log('Uppercasing...');
+  sleep(1);
+  uppercased = calculated.toUpperCase();
+  console.log(uppercased)
+});
+
+// Later in runtime
+
+b.preform();
+
+
+// Blocking node sleep function
+function sleep(n) {
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n * 1000);
+}
